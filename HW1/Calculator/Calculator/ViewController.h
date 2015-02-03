eb1498bc9d59f368add27fa1ad7e28521b21fd5f
Quyen Nguyen <Quyen@Quyens-Mac-Pro.local>
2015-02-02 20:43:13 -0700

    HW1

---
+++ HW1/Calculator/Calculator/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Quyen Nguyen on 1/31/15.
+//  Copyright (c) 2015 Quyen Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+@end
+
